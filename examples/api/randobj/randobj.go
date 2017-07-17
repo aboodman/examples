4fdcfcb32dc365b1a59d459264324cfb30ef3445
Aaron Boodman <aaron@aaronboodman.com>
2017-07-17 02:49:33 -0700

    Simple program to demonstrate IPNS usage

--- examples/api/randobj/randobj.go
+++ examples/api/randobj/randobj.go
@@ -1,35 +1,20 @@
-## Random Ipfs Objects
-During the development, ive frequently found the need to just get a hash of some
-random ipfs object. At first, I would just ask in irc "can someone give me a
-hash?". But I decided I could do better, So I decided to make it a service. In
-this article, im going to go over how I did that (hint: its really simple!)
-
-First, lets get some imports:
-```
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 
 	u "github.com/ipfs/go-ipfs-util"
 	"github.com/ipfs/go-ipfs/core"
 	"github.com/ipfs/go-ipfs/core/coreunix"
+	"github.com/ipfs/go-ipfs/path"
 	"github.com/ipfs/go-ipfs/repo/fsrepo"
 	"golang.org/x/net/context"
 )
-```
 
-This just pulls in some basic ipfs packages, and the default golang http server.
-Now, since im lazy, im going to have a global for our ipfsnode.
-
-```
 var gnode *core.IpfsNode
-```
 
-Now, lets write the http handler func for generating our random objects.
-
-```
 func ServeIpfsRand(w http.ResponseWriter, r *http.Request) {
 	read := io.LimitReader(u.NewTimeSeededRand(), 2048)
 
@@ -40,32 +25,33 @@ func ServeIpfsRand(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.Write([]byte(str))
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	name := "foo" // assumes you've created a key w/ this name using cil
+	pk, err := gnode.GetKey(name)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("publishing", str, "to", name)
+	// We hang at this line
+	err = gnode.Namesys.Publish(ctx, pk, path.Path(str))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("done")
 }
-```
 
-And now, lets tie it all together in a main function.
-
-Set up our node configuration, and use the users standard ipfs configuration directory.
-
-```
 func main() {
 	r, err := fsrepo.Open("~/.ipfs")
 	if err != nil {
 		panic(err)
 	}
-```
 
-Now we need to set up our context
-
-```
 	// Make our 'master' context and defer cancelling it
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-```
 
-Then create a configuration and finally create our node!
-
-```
 	cfg := &core.BuildCfg{
 		Repo:   r,
 		Online: true,
@@ -82,6 +68,3 @@ Then create a configuration and finally create our node!
 	http.HandleFunc("/ipfsobject", ServeIpfsRand)
 	http.ListenAndServe(":8080", nil)
 }
-```
-
-By [whyrusleeping](http://github.com/whyrusleeping)
